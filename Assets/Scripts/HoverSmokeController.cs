e16dc7be15440fc2a920798aeab5f59222eb1bec
guins94 <guins94@hotmail.com>
2023-06-23 15:24:42 -0300

    Hover Smoke Particle Scripts and Prefabs Creation
    Hover Smoke Controller has the audio source reference of the smoke and reference for the particle system.

---
+++ Assets/Scripts/HoverSmokeController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverSmokeController : MonoBehaviour {
+    public bool hover = false;
+    public AudioSource audioSource;
+    public ParticleSystem hoverParticles;
+
+	void Start () 
+    {
+        audioSource = GetComponent<AudioSource>();
+        hoverParticles = GetComponent<ParticleSystem>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (GameLogic.instance.gameState != GameLogic.GameStates.gameplay)
+        {
+            return;
+        }
+        if (hover)
+        {
+            if (audioSource != null && !audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+            if (hoverParticles != null ) hoverParticles.Play();
+        }
+        else
+        {
+            if (audioSource != null && audioSource.isPlaying)
+            {
+                audioSource.Stop();
+                if (hoverParticles != null ) hoverParticles.Stop();
+            }
+        }
+	}
+}
